cb05575b583703fae40f3da72af9b71f8d795f29
Ana Maria Vergara Posada <anamariavergaraposada@Anas-MacBook-Air.local>
2023-09-25 11:24:13 -0500

    Basic examples

---
+++ ejemploLogica.py
@@ -0,0 +1,9 @@
+# Solicitar al usuario dos números
+numero1 = float(input("Ingrese el primer número: "))
+numero2 = float(input("Ingrese el segundo número: "))
+
+# Sumar los dos números
+resultado = numero1 + numero2
+
+# Mostrar el resultado
+print("La suma de", numero1, "y", numero2, "es igual a", resultado)
